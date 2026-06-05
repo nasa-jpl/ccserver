a93be907ab4130cf46339614e9c120f989865597
Adit Sahasrabudhe <adit.sahasrabudhe@jpl.nasa.gov>
2026-06-04 17:07:16 -0700

    Initial CORECONF server

--- include/coreconf_handlers.h
+++ include/coreconf_handlers.h
@@ -2,6 +2,7 @@
 #define CORECONF_HANDLERS_H
 
 #include <coap3/coap.h>
+
 #include <cstdint>
 #include <vector>
 
@@ -14,34 +15,31 @@ namespace coreconf {
 void initialize_handlers(coap_context_t *ctx);
 
 /**
+ * Cleanup CORECONF resources
+ * Should be called before program exit to free allocated memory
+ */
+void cleanup_handlers();
+
+/**
  * Handler for CoAP GET requests
  * Maps to CORECONF GET operation for retrieving single data nodes
  */
-void handle_get(coap_resource_t *resource,
-                coap_session_t *session,
-                const coap_pdu_t *request,
-                const coap_string_t *query,
-                coap_pdu_t *response);
+void handle_get(coap_resource_t *resource, coap_session_t *session, const coap_pdu_t *request,
+                const coap_string_t *query, coap_pdu_t *response);
 
 /**
  * Handler for CoAP FETCH requests (RFC 8132)
  * Maps to CORECONF FETCH operation for retrieving multiple data nodes
  */
-void handle_fetch(coap_resource_t *resource,
-                  coap_session_t *session,
-                  const coap_pdu_t *request,
-                  const coap_string_t *query,
-                  coap_pdu_t *response);
+void handle_fetch(coap_resource_t *resource, coap_session_t *session, const coap_pdu_t *request,
+                  const coap_string_t *query, coap_pdu_t *response);
 
 /**
  * Handler for CoAP iPATCH requests (RFC 8132)
  * Maps to CORECONF iPATCH operation for partial updates
  */
-void handle_ipatch(coap_resource_t *resource,
-                   coap_session_t *session,
-                   const coap_pdu_t *request,
-                   const coap_string_t *query,
-                   coap_pdu_t *response);
+void handle_ipatch(coap_resource_t *resource, coap_session_t *session, const coap_pdu_t *request,
+                   const coap_string_t *query, coap_pdu_t *response);
 
 /**
  * Parse CBOR payload from request
@@ -58,6 +56,6 @@ bool parse_cbor_request(const uint8_t *data, size_t size);
  */
 size_t encode_cbor_response(std::vector<uint8_t> &buffer);
 
-} // namespace coreconf
+}  // namespace coreconf
 
-#endif // CORECONF_HANDLERS_H
+#endif  // CORECONF_HANDLERS_H
