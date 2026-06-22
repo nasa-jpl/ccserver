be7eb93a345ab83e2ac23e103337f1b7dc5639c9
Adit Sahasrabudhe <adit.sahasrabudhe@jpl.nasa.gov>
2026-06-22 12:25:11 -0700

    Updated simple-example to use more prefixes in the handler names

--- impl/simple-example-impl.h
+++ impl/simple-example-impl.h
@@ -6,21 +6,24 @@
 #include <stdbool.h>
 #include <string.h>
 #include <coreconfTypes.h>
-#define read_dataStore_dataStoreValue read_1002
-#define write_dataStore_dataStoreValue write_1002
-uint32_t read_dataStore_dataStoreValue(void);
-#define read_dataStore_itemList read_1003
-#define write_dataStore_itemList write_1003
-CoreconfValueT* read_dataStore_itemList(uint32_t itemList_index);
-#define read_itemList_subItemList read_1005
-#define write_itemList_subItemList write_1005
-CoreconfValueT* read_itemList_subItemList(uint32_t subItemList_index);
-#define read_subItemList_subValue read_1007
-#define write_subItemList_subValue write_1007
-int32_t read_subItemList_subValue(void);
-#define read_itemList_value read_1008
-#define write_itemList_value write_1008
-int32_t read_itemList_value(void);
+#define read_simpleExample_dataStore read_1001
+#define write_simpleExample_dataStore write_1001
+CoreconfValueT* read_simpleExample_dataStore(void);
+#define read_simpleExample_dataStore_dataStoreValue read_1002
+#define write_simpleExample_dataStore_dataStoreValue write_1002
+uint64_t read_simpleExample_dataStore_dataStoreValue(void);
+#define read_simpleExample_dataStore_itemList read_1003
+#define write_simpleExample_dataStore_itemList write_1003
+CoreconfValueT* read_simpleExample_dataStore_itemList(uint64_t dataStore_itemList_index);
+#define read_dataStore_itemList_subItemList read_1005
+#define write_dataStore_itemList_subItemList write_1005
+CoreconfValueT* read_dataStore_itemList_subItemList(uint64_t itemList_subItemList_index);
+#define read_itemList_subItemList_subValue read_1007
+#define write_itemList_subItemList_subValue write_1007
+int64_t read_itemList_subItemList_subValue(void);
+#define read_dataStore_itemList_value read_1008
+#define write_dataStore_itemList_value write_1008
+int64_t read_dataStore_itemList_value(void);
 
 
 // User-facing read/write function prototypes
